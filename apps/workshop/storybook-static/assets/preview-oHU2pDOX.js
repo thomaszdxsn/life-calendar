cc695fd89105c8ce9efae22347ea41365e4bf75c
thomaszdxsn <thomaszdxsn@icloud.com>
2023-12-31 19:09:53 +0800

    fix: remove storybook-static folder

--- apps/workshop/storybook-static/assets/preview-oHU2pDOX.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-6A_P1erF.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-C1U_FbR3.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-C1U_FbR3.js","./iframe-6A_P1erF.js","./index-XiNr8FW2.js","./_commonjsHelpers-5-cIlDoe.js","./react-18-8u3SsdfO.js","./index-ogXoivrg.js","./_getPrototype-4FkL748a.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
